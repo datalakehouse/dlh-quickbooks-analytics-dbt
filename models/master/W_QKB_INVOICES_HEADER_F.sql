b26dc5cd6a2a8106637011b45c234b83ab0cb3d9
SMILTON_ROA <smilton_roa@zenvia.com>
2022-10-13 16:03:59 +0200

    Add invoice header fact table

---
+++ models/master/W_QKB_INVOICES_HEADER_F.sql
@@ -0,0 +1,13 @@
+{{ config (
+  materialized= 'table',
+  schema=var('target_schema', 'QUICKBOOKS'),
+  tags= ["staging", "daily"],
+  transient=false
+)
+}}
+
+
+SELECT
+  *
+FROM
+  {{ref('V_QKB_INVOICES_HEADER_STG')}} AS C
